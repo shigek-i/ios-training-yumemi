86909ed681b3a5cd7d64e3826cee5d2c501f639d
s12private <android.12.device@gmail.com>
2021-08-07 22:50:10 +0900

    Initial Commit

---
+++ ios-training-yumemi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-training-yumemi
+//
+//  Created by 松中誉生 on 2021/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
