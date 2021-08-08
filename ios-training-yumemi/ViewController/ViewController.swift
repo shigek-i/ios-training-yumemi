b482e8e948a4791f894bfac819e72056d9741ab5
s12private <android.12.device@gmail.com>
2021-08-09 03:03:42 +0900

    add: Storyboardとコードを接続

--- ios-training-yumemi/ViewController/ViewController.swift
+++ ios-training-yumemi/ViewController/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var reloadButton: UIButton!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func reloadButtonTapped(_ sender: Any) {
+    }
 }
